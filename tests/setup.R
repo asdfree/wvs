668656a9781d46be2026ac52a2f1662879128a19
Anthony Damico <ajdamico@gmail.com>
2017-11-12 03:19:19 -0500

    fixes https://github.com/ajdamico/asdfree/issues/308

--- tests/setup.R
+++ tests/setup.R
@@ -1,15 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+this_sample_break <- Sys.getenv( "this_sample_break" )
+
 library(lodown)
 
 wvs_cat <-
 	get_catalog( "wvs" ,
 		output_dir = file.path( getwd() ) )
 
-# sample 25% of the records
-which_records <- sample( seq( nrow( wvs_cat ) ) , round( nrow( wvs_cat ) * 0.25 ) )
+record_categories <- ceiling( seq( nrow( wvs_cat ) ) / ceiling( nrow( wvs_cat ) / 5 ) )
 
-# always sample the united states wave == 6 file
-wvs_cat <- unique( rbind( wvs_cat[ which_records , ] , subset( wvs_cat , grepl( "United(.*)States" , full_url ) & wave == 6 ) ) )
+wvs_cat <- unique( rbind( wvs_cat[ record_categories == this_sample_break , ] , wvs_cat[ grepl( "United(.*)States" , wvs_cat$full_url ) & wvs_cat$wave == 6 , ] ) )
 
 lodown( "wvs" , wvs_cat )
