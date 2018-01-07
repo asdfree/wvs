928bbd1aa3e10db321df8b09920b7e1fbf126486
Anthony Damico <ajdamico@gmail.com>
2018-01-07 16:36:43 -0500

    output_dir

--- tests/setup.R
+++ tests/setup.R
@@ -3,7 +3,7 @@ if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 library(lodown)
 lodown( "wvs" , output_dir = file.path( getwd() ) )
 this_sample_break <- Sys.getenv( "this_sample_break" )
-wvs_cat <- get_catalog( "wvs" , wvs_cat )
+wvs_cat <- get_catalog( "wvs" , output_dir = file.path( getwd() ) )
 record_categories <- ceiling( seq( nrow( wvs_cat ) ) / ceiling( nrow( wvs_cat ) / 5 ) )
 wvs_cat <- wvs_cat[ record_categories == this_sample_break , ]
 lodown( "wvs" , wvs_cat )
